437097d722db34b0b614760947385ae0b17412eb
John Soupir <johnsoupir@gmail.com>
2023-11-18 10:46:56 -0600

    Moves LLM and TTS to remote server

--- Assistant/Client/ai.py
+++ Assistant/Client/ai.py
@@ -3,6 +3,7 @@ import argparse
 import queue
 import sys
 from time import sleep
+import requests
 import sounddevice as sd
 from vosk import Model, KaldiRecognizer
 from gpt4all import GPT4All
@@ -14,11 +15,27 @@ from llama_cpp import Llama
 from google.cloud import texttospeech
 import openai
 from simpleMQTT import create_mqtt_client, connect_to_broker, publish_message, disconnect_broker
+import re
+import asyncio
+from playsound import playsound
+import threading
+
+
+def removeEmojis(text):
+    # Define the emoji pattern
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
 
 
 def setOpenAILocal():
     openai.api_key = "..."
-    openai.api_base = "http://allevil.local:5000/v1"
+    openai.api_base = "http://192.168.192.201:5000/v1"
+    # openai.api_base = "http://allevil.local:5000/v1"
     openai.api_version = "2023-05-15"
 
 def sendMQTT(state):
@@ -44,7 +61,8 @@ def load_KEY(keyfile):
 
 def promptOpenAI(input):
     summary = openai.ChatCompletion.create(
-        model='llama-2-7b-chat.Q4_0.gguf',
+        model='gpt-3.5-turbo-16k',
+        # model='llama-2-7b-chat.Q4_0.gguf',
         messages=[{"role":"user", "content": input}]
     )
     return summary.choices[0].message.content + " "
@@ -93,6 +111,28 @@ def callback(indata, frames, time, status):
     if not audioPlaying:  # Check if audio is not playing before adding to the queue
         q.put(bytes(indata))
 
+def serverTTS(text, server_url="http://192.168.192.201:4999/synthesize", output_file='out.wav'):
+# def serverTTS(text, server_url="http://allevil.local:4999/synthesize", output_file='out.wav'):
+    """
+    Sends text to a server to be converted to speech and saves the returned audio.
+
+    Args:
+    text (str): The text to be converted to speech.
+    server_url (str): The URL of the server providing the text-to-speech service.
+    output_file (str): The path where the output audio file will be saved.
+    """
+    try:
+        response = requests.post(server_url, data=text)
+
+        if response.status_code == 200:
+            with open(output_file, 'wb') as audio_file:
+                audio_file.write(response.content)
+            print(f"Audio saved as {output_file}.")
+        else:
+            print(f"Error: Server responded with status code {response.status_code}")
+
+    except requests.RequestException as e:
+        print(f"An error occurred while making the request: {e}")
 
 # def callback(indata, frames, time, status):
 #     """This is called (from a separate thread) for each audio block."""
@@ -110,12 +150,16 @@ mqtt_client = create_mqtt_client()
 # FourAllmodel = GPT4All("/home/john/.local/share/nomic.ai/GPT4All/mistral-7b-openorca.Q4_0.gguf", allow_download=False)
 # llm = Llama(model_path="./llama-2-7b-chat.Q4_0.gguf")
 
-setOpenAILocal()
+# setOpenAILocal()
 
 # print(openai.ChatCompletion.create("How are you"))
-print(promptOpenAI("Hello, how are you?"))
-print("DONE.")
-sleep(10)
+# print("Prompting API....")
+#testResponse = promptOpenAI("Hello, how are you?")
+#print(testResponse)
+#print("\n\n\n Req Speech \n\n\n")
+#serverTTS(testResponse)
+#playAudio("out.wav")
+#print("DONE.")
 model_name = 'tts_models/en/ljspeech/tacotron2-DDC'
 tts = TTS(model_name)
 # tts.tts_to_file(text=inputText, file_path=outputFile)
@@ -145,6 +189,7 @@ parser.add_argument(
     "-m", "--model", type=str, help="language model; e.g. en-us, fr, nl; default is en-us")
 args = parser.parse_args(remaining)
 
+systemPrompt = "You are a home automation AI. If you are asked a question you will give a helpful answer. If you are asked to control a device, you will respond with Command Detected: <device> <state>.  User: "
 
 try:
     if args.samplerate is None:
@@ -188,17 +233,32 @@ try:
                 # print("Final output is: ", userSpeech)
                 if (userSpeech != ""):
                     audioPlaying = True
-                    playAudio("processing.mp3")
+                   # playAudio("processing.mp3")
                     # inputToLLM = "Q: " + userSpeech  + " A:"
                     # print(inputToLLM)
                     #outputText = llm(inputToLLM, max_tokens=48, stop=["Q:", "\n"], echo=True)
                     print('User: ' + userSpeech)
-                    if "on" in userSpeech:
-                        sendMQTT('1')
-                        pass
-                    if "off" in userSpeech:
-                        sendMQTT('0')
+
+#                    if "on" in userSpeech:
+#                        sendMQTT('1')
+#                        pass
+#                    if "off" in userSpeech:
+#                        sendMQTT('0')
+
                     # userSpeech += "?"
+                    answer = promptOpenAI(systemPrompt + userSpeech)
+                    answer = removeEmojis(answer)
+                    googleTTS(answer, "out.wav")
+                    # serverTTS(answer)
+                    playAudio("out.wav")
+
+                    if "command detected" in answer.lower():
+                        if "on" in answer.lower():
+                            print("\n\n LIGHT ON \n\n")
+                            sendMQTT('1')
+                        if "off" in answer.lower():
+                            print("\n\n LIGHT OFF \n\n")
+                            sendMQTT('0')
                     # answer = FourAllmodel.generate(userSpeech, max_tokens=200)
                     #FourAllmodel.chat_session.append()
                     # answer = promptOpenAI(userSpeech)

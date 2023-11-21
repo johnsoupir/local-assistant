47592d84c26aa08c75c8631870e7e7bbd8eb7d82
John Soupir <johnsoupir@gmail.com>
2023-11-20 20:18:09 -0600

    Fixes model path

--- Assistant/Server/runLLMServer.sh
+++ Assistant/Server/runLLMServer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd text-generation-webui
-./start_linux.sh --listen --api --model llama-2-7b-chat.Q4_K_M.gguf
+./start_linux.sh --listen --api --model models/llama-2-7b-chat.Q4_K_M.gguf
 
 

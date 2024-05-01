28e8623f84312d0ed3064f16e313631b325372f7
John Soupir <johnsoupir@gmail.com>
2024-04-30 21:07:34 -0500

    Improves TTS quality

--- Assistant/Server/TTS/server.py
+++ Assistant/Server/TTS/server.py
@@ -4,7 +4,9 @@ from TTS.api import TTS
 app = Flask(__name__)
 
 # Initialize the TTS object with the specified model
-tts = TTS(model_name="tts_models/en/ljspeech/fast_pitch", progress_bar=True, gpu=True)
+# tts = TTS(model_name="tts_models/en/ljspeech/fast_pitch", progress_bar=True, gpu=True)
+# tts = TTS(model_name="tts_models/en/vctk/vits" , progress_bar=True, gpu=True) #.to("cuda")
+tts = TTS(model_name="tts_models/en/vctk/vits").to("cuda")
 
 @app.route('/synthesize', methods=['POST'])
 def synthesize():
@@ -12,9 +14,17 @@ def synthesize():
     output_path = 'OUTPUT.wav'
     
     # Generate speech and save to file
-    tts.tts_to_file(text, file_path=output_path)
+   # tts.tts_to_file(text, file_path=output_path, speaker="p267")
+    tts.tts_to_file(text, file_path=output_path, speaker="p234")
     
     return send_file(output_path, as_attachment=True)
 
+
+@app.route('/audioresponse', methods=['POST'])
+def audioresponse():
+    userPrompt = request.data.decode()
+    #PROMPT TTS. 
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4999)
+

25673e7eee76c8ba89086ae8e7f4555ef93ed2ae
John Soupir <johnsoupir@gmail.com>
2024-04-30 21:06:54 -0500

    Fixes setup script

--- Assistant/Server/setup.sh
+++ Assistant/Server/setup.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-sudo apt install espeak
+sudo apt install -y espeak
 
 
 #Install python reqs
+python3 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
 
-#Install python 
-git clone --recurse-submodules https://github.com/johnsoupir/llama-cpp-python-pascal.git
-cd llama-cpp-python-pascal
+#Install python bindings of llama.cpp
+NEW_GPU="https://github.com/abetlen/llama-cpp-python.git"
+PASCAL="https://github.com/johnsoupir/llama-cpp-python-pascal.git"
 
-#clone text-gen-webui 
-git clone https://github.com/oobabooga/text-generation-webui.git
+echo "#### Do you have a Pascal GPU? P40, P100, etc.."
+echo "Enter y or n: "
+read GPU_TYPE
 
+if [[ $GPU_TYPE == "y" ]]
+then 
+    echo "PASCAL INSTALL"
+    git clone --recurse-submodules $PASCAL
+    cd llama-cpp-python-pascal
+else
+    echo "NORMAL INSTALL"
+    pip install llama-cpp-python
+fi
+
+
+
+#Download default model
 echo "DOWNLOADING LLM MODEL"
-cd text-generation-webui/models
-wget https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/blob/main/llama-2-7b-chat.Q4_K_M.gguf
+wget https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/blob/main/llama-2-7b-chat.Q4_K_M.ggufhttps://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_K_M.gguf
 
-cd ..
-
-echo "\n\n\n"
-echo "TEXT GEN WEBUI WILL BE SET UP NEXT. WHEN PROMPTED SELECT YOUR GPU TYPE."
-echo "ENTER TO CONTINUE"
-echo "\n\n\n"
-read 
-
-
-./start_linux.sh --listen --api --model modelspython -m bark --text "Hello, my name is Suno." --output_filename "example.wav"/llama-2-7b-chat.Q4_K_M.gguf
